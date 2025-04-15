38f3fa4cabd6325581b74eff65e4b1c16fc850f3
michaele490 <michaelegharevba490@gmail.com>
2025-04-16 00:43:18 +0100

    Added filtering of personal and sensitive data in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,18 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  # Default sensitive parameters
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+  
+  # Personal identification information
+  :first_name, :last_name, :dob, :phone_number,
+  :emergency_contact_first_name, :emergency_contact_last_name, 
+  :emergency_contact_phone_number,
+  
+  # Sensitive medical information
+  :diagnosis, :treatment, :next_appointment_date,
+  
+  # Additional security parameters
+  :remember_token, :reset_password_token, :confirmation_token,
+  :unlock_token, :authentication_token
 ]

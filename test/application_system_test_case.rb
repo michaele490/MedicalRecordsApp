363f2e18613d1502fac804842f801cf22e88d95b
michaele490 <michaelegharevba490@gmail.com>
2025-04-20 11:20:06 +0100

    First systems test configuration that is working

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,15 @@
 require "test_helper"
+require "selenium-webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |options|
+    options.add_argument("--headless")
+    
+    # Set ChromeDriver path using Windows path format
+    Selenium::WebDriver::Chrome::Service.driver_path = "C:\\chromedriver.exe"
+    
+    # Additional Chrome options for stability
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+  end
 end
--- test/system/admins_test.rb
+++ test/system/admins_test.rb
@@ -1,60 +1,8 @@
 require "application_system_test_case"
 
 class AdminsTest < ApplicationSystemTestCase
-  # This test must run with no admins in the database
-  test 'staff page is admin only redirect to sign up' do
-    visit staff_path
-    assert_current_path new_admin_registration_path
-    assert_selector "[role='alert']", text: "Please create an admin account first."
+  test "visiting the home page" do
+    visit root_path
+    assert_selector "h1", text: "Medical Record Management System"
   end
-
-  # Setup for all other tests that need an admin
-  setup do
-    # References fixtures yml files
-    @admin = admins(:one)
-  end
-
-  # Redirects to sign up when there are no admins in the database
-=begin
-  test 'staff page is admin only, non-admin is redirected to sign up' do
-    visit staff_path
-    assert_current_path new_admin_registration_path
-    assert_selector "[role='alert']", text: "Please create an admin account first."
-  end
-=end
-
-=begin
-  test "admin can sign in" do
-    visit new_admin_session_path
-    fill_in "Email", with: @admin.email
-    fill_in "Password", with: 'password'  # We'll set this in the fixture
-    click_on "Log in"
-    
-    assert_text "Signed in successfully"
-    assert_current_path root_path
-  end
-
-  test "admin cannot sign in with invalid credentials" do
-    visit new_admin_session_path
-    fill_in "Email", with: @admin.email
-    fill_in "Password", with: 'wrong_password'
-    click_on "Log in"
-    
-    assert_text "Invalid Email or password"
-    assert_current_path new_admin_session_path
-  end
-
-  test "admin can sign out" do
-    # First sign in
-    visit new_admin_session_path
-    fill_in "Email", with: @admin.email
-    fill_in "Password", with: 'password'
-    click_on "Log in"
-    
-    # Then sign out
-    click_on "Sign out"
-    assert_text "Signed out successfully"
-    assert_current_path root_path
-  end
-=end
 end 
